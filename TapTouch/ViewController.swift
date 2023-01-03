63eb1905a8fa4eb3b684b938596f2b4347fc641f
doomout <winquake3@hanmail.net>
2023-01-03 15:16:27 +0900

    코드 정리

--- TapTouch/ViewController.swift
+++ TapTouch/ViewController.swift
@@ -7,7 +7,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
