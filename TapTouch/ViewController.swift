cc99b49dc1f9d7547e275f6d6db9b4ea8487d263
doomout <winquake3@hanmail.net>
2023-01-03 14:56:57 +0900

    터치, 탭 기능 구현,  readme update

--- TapTouch/ViewController.swift
+++ TapTouch/ViewController.swift
@@ -10,6 +10,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch //현재 발생한 터치 이벤트를 가지고 온다
+        
+        txtMessage.text = "Touches Began" //현재 발생한 이벤트의 종류를 출력한다.
+        txtTapCount.text = String(touch.tapCount) //터치의 개수를 출력한다.
+        txtTouchCount.text = String(touches.count) //탭의 개수를 출력한다.
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch //현재 발생한 터치 이벤트를 가지고 온다
+        
+        txtMessage.text = "Touches Moved" //현재 발생한 이벤트의 종류를 출력한다.
+        txtTapCount.text = String(touch.tapCount) //터치의 개수를 출력한다.
+        txtTouchCount.text = String(touches.count) //탭의 개수를 출력한다.
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch //현재 발생한 터치 이벤트를 가지고 온다
+        
+        txtMessage.text = "Touches Ended" //현재 발생한 이벤트의 종류를 출력한다.
+        txtTapCount.text = String(touch.tapCount) //터치의 개수를 출력한다.
+        txtTouchCount.text = String(touches.count) //탭의 개수를 출력한다.
+    }
 }
 
